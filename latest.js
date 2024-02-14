f8f07744155ba47748b76aa54b8c25cdbc98f1a1
Esteban <esteban@fwd.dev>
2024-02-14 14:36:00 -0500

    fix css

--- latest.js
+++ latest.js
@@ -530,7 +530,7 @@
 							<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 6V2c0-1.1.9-2 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-4v4a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h4zm2 0h4a2 2 0 0 1 2 2v4h4V2H8v4zM2 8v10h10V8H2z"/></svg>
 						</div>
 						<div class="nano-pay-copy-clipboard" onclick="window.NanoPay.CopyToClipboard('${rpc_checkout.amount}', this)">
-							<span>${rpc_checkout.amount} ${symbol}</span>
+							<span>${String(rpc_checkout.amount).slice(0, 10)} ${symbol}</span>
 							<svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 6V2c0-1.1.9-2 2-2h10a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-4v4a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h4zm2 0h4a2 2 0 0 1 2 2v4h4V2H8v4zM2 8v10h10V8H2z"/></svg>
 						</div>
 					</div>
